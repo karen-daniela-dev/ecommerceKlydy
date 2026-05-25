3dbee3070a866342d90aa7e7cd1e8a06726c692c
KarenDiaz <calidad8844@gmail.com>
2026-05-25 12:26:16 -0500

    first commit

---
+++ src/main/java/com/ecommerce/klydy/KlydyApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.klydy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlydyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlydyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/klydy/KlydyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.klydy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlydyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
