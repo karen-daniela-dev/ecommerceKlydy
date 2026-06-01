80d3b8ac99824c83f16a03b3c55fa61ac8c0d679
KarenDiaz <calidad8844@gmail.com>
2026-06-01 13:29:21 -0500

    se protegen rutas para admin

--- src/main/java/com/ecommerce/klydy/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/klydy/config/SecurityConfig.java
@@ -46,11 +46,11 @@ public class SecurityConfig {
                         ).permitAll()
 
 
-                        // TEMPORAL: abierto mientras no hay autenticación
-                        .requestMatchers(HttpMethod.GET,    "/productos/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/productos/**").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/productos/**").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/productos/**").permitAll()
+//                        // TEMPORAL: abierto mientras no hay autenticación
+//                        .requestMatchers(HttpMethod.GET,    "/productos/**").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/productos/**").permitAll()
+//                        .requestMatchers(HttpMethod.PUT, "/productos/**").permitAll()
+//                        .requestMatchers(HttpMethod.DELETE, "/productos/**").permitAll()
 
                         // Endpoints de autenticación: públicos — son la puerta de entrada.
                         .requestMatchers("/auth/**").permitAll()
@@ -61,13 +61,13 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/compras/**").hasAnyRole("ADMIN", "CLIENTE")
 
                         // Modificaciones: solo ADMIN puede crear, actualizar o eliminar.
-//                        .requestMatchers(HttpMethod.POST, "/productos/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.PUT, "/productos/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.DELETE, "/productos/**").hasRole("ADMIN")
-//
-//                        .requestMatchers(HttpMethod.POST, "/clientes/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.PUT, "/clientes/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.DELETE, "/clientes/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/productos/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/productos/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/productos/**").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.POST, "/clientes/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/clientes/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/clientes/**").hasRole("ADMIN")
 
                         // Órdenes: CLIENTE puede crear y ver sus propias órdenes.
                         .requestMatchers(HttpMethod.POST, "/compras/**").hasAnyRole("ADMIN", "CLIENTE")
