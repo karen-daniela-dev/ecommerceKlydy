81a83b2cbe2d67df78de6dccb960d09eb5000a11
KarenDiaz <calidad8844@gmail.com>
2026-06-06 22:43:37 -0500

    se modifica modelo gemini para permitir el deploy

--- src/main/java/com/ecommerce/klydy/service/ChatService.java
+++ src/main/java/com/ecommerce/klydy/service/ChatService.java
@@ -20,7 +20,7 @@ public class ChatService {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     private static final String GEMINI_URL =
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=";
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=";
 
     private static final String BACKEND_PRODUCTOS_URL =
             "https://ecommerceklydy.onrender.com/productos";
